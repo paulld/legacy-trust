38afc65c822978983484dbfb34066a6a07784914
Paul <paul.lugagnedelpon@gmail.com>
2015-04-24 10:15:33 +0800

    Fix the functions after loading, vertical center text

--- app/assets/javascripts/angular/controllers/home_controller.js
+++ app/assets/javascripts/angular/controllers/home_controller.js
@@ -1,3 +1,11 @@
-app.controller('HomeCtrl', ['$scope', function($scope){
+app.controller('HomeCtrl', ['$scope', '$interval', 'Afterload', function($scope, $interval, Afterload){
+
+  $scope.$watch('$viewContentLoaded', function() {
+    $interval(function(){
+      if (!$('.main-container section:first-child').hasClass('first-child')) {
+        Afterload();
+      }
+    }, 100, 10);
+  });
 
 }]);
