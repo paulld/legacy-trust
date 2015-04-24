9e5c5a9977ab3e5e592cbcaa38ce25c7216630c6
Paul <paul.lugagnedelpon@gmail.com>
2015-04-24 12:06:55 +0800

    Add theme specific css; fix some responsiveness rules

--- app/assets/javascripts/angular/controllers/head_nav_controller.js
+++ app/assets/javascripts/angular/controllers/head_nav_controller.js
@@ -3,5 +3,9 @@ app.controller('HeadNavCtrl', ['$scope', '$location', function($scope, $location
   $scope.isActive = function(route) {
     return (route === $location.path())? 'active' : '';
   };
+
+  $scope.openNav = function () {    
+    $('nav').toggleClass('open-nav');
+  }
   
 }]);
