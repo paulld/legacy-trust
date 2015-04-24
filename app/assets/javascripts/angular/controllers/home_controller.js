a05473b14d589c9cc8f988dc42f3307bc73534c6
Paul <paul.lugagnedelpon@gmail.com>
2015-04-24 11:16:56 +0800

    manually add padding-top for first section

--- app/assets/javascripts/angular/controllers/home_controller.js
+++ app/assets/javascripts/angular/controllers/home_controller.js
@@ -1,15 +1,14 @@
 app.controller('HomeCtrl', ['$scope', '$interval', 'Afterload', function($scope, $interval, Afterload){
 
   $scope.$watch('$viewContentLoaded', function() {
-    var x = 0;
+    // var x = 0;
     $interval(function(){
-      // Afterload();
-      x++;
+      // x++;
       if (!$('.main-container section:first-child').hasClass('first-child')) {
         Afterload();
-        console.log('DONE', x);
-      } else {
-        console.log('already done', x);
+        // console.log('DONE', x);
+      // } else {
+        // console.log('already done', x);
       }
       $('.slides li.overlay').each(function(){
         console.log('hi');
